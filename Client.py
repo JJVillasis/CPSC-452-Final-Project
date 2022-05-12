89488e5581a3c91c3f91ccc166332686e9351806
Joshua Villasis <JJVillasis@csu.fullerton.edu>
2022-05-11 22:59:41 -0700

    Slightly improved Server and Client files

--- Client.py
+++ Client.py
@@ -1,13 +1,21 @@
 import socket
 
-HOST = 'localhost'    # The remote host
-PORT = 50007              # The same port as used by the server
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-	s.connect((HOST, PORT))
-	userInput = input("Send Something to the Host: ")
-	s.sendall(userInput.encode("utf-8"))
-	data = s.recv(1024)
+HOST = '127.0.0.1'    # The remote host
+PORT = 57000              # The same port as used by the server
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    #Connect to Host
+    sock.connect((HOST, PORT))
 
-	if data:
-	    dataD = data.decode("utf-8")
-	    print('Received:', dataD)
+    #Event Loop
+    while True:
+        userInput = input("Send Something to the Host: ")
+        sock.sendall(userInput.encode("utf-8"))
+        data = sock.recv(1024)
+
+        if data.decode("utf-8").lower() == "goodbye":
+            print(f"Connection ended with {HOST}. Goodbye")
+            break
+
+        if data:
+            dataD = data.decode("utf-8")
+            print('Received:', dataD)
--- Server.py
+++ Server.py
@@ -1,54 +1,77 @@
-import select, socket, sys, queue
+import selectors
+import socket
+import sys
+import types
 
-HOST = ''                 # Symbolic name meaning all available interfaces
-PORT = 50007              # Arbitrary non-privileged port
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-    server.setblocking(0)
-    server.bind((HOST, PORT))
-    server.listen(5)
-    inputs = [server]
-    outputs = []
-    message_queues = {}
+sel = selectors.DefaultSelector()
 
-    while inputs:
-        readable, writable, exceptional = select.select(inputs, outputs, inputs)
+#Creates new socket and adds it to selector
+def accept_wrapper(sock):
+    conn, addr = sock.accept()
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr = addr, inb = b"", outb = b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data = data)
 
-        for read in readable:
-            if read is server:
-                connection, client_address = read.accept()
-                connection.setblocking(0)
-                inputs.append(connection)
-                message_queues[connection] = queue.Queue()
+#Handles client connections
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    #Socket is ready to read Client's data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)
+        #Data received from Client
+        if recv_data:
+            data.outb += recv_data
+        #No data received from Client
+        else:
+            print(f"Closing connection to {data.addr}")
+            sel.unregister(sock)
+            sock.close()
 
+    #Echo received data back to Client
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print(f"Echoing {data.outb!r} to {data.addr}")
+            sent = sock.send(data.outb)
+            data.outb = data.outb[sent:]
+
+#Check if user entered correct amount of commands
+if len(sys.argv) != 3:
+    print("ERROR: User must specify <HOST ADDRESS> and <PORT NUMBER>.")
+    print()
+    exit(-1)
+
+#Get host address, and port number
+host, port = sys.argv[1], int(sys.argv[2])
+
+#TESTING - Print entered host and port
+print("Host address:", host)
+print("Port number:", port)
+print()
+
+#Create listening socket
+hostSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+hostSock.bind((host, port))
+hostSock.listen()
+print(f"Listening on {(host, port)}")
+hostSock.setblocking(False)
+sel.register(hostSock, selectors.EVENT_READ, data=None)
+
+#Host Server Event Loop
+try:
+    while True:
+        events = sel.select(timeout = None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
             else:
-                data = read.recv(1024)
+                service_connection(key, mask)
 
-                if data.decode("utf-8") == "shutdown":
-                    server.close()
-                    exit(1)
-
-                elif data:
-                    message_queues[read].put(data)
-                    if read not in outputs:
-                        outputs.append(read)
-
-                else:
-                    if read in outputs:
-                        outputs.remove(read)
-                        read.close()
-                        del message_queues[read]
-
-        for write in writable:
-            try:
-                next_msg = message_queues[write].get_nowait()
-            except queue.Empty:
-                outputs.remove(write)
-            else:
-                write.send(next_msg)
-
-        for exception in exceptional:
-            inputs.remove(exception)
-            if exception in outputs:
-                outputs.remove(exception)
-            exception.close
-            del message_queues[exception]
+                if key.data.outb:
+                    print(f"Testing: {key.data.outb}")
+except KeyboardInterrupt:
+    print("Caught keyboard interrupt, exiting")
+finally:
+    sel.close()
