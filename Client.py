41a600779cf5cfb5252fbdca27ca6eb124f00e42
Joshua Villasis <JJVillasis@csu.fullerton.edu>
2022-05-13 16:09:01 -0700

    Remade Server.py

--- Client.py
+++ Client.py
@@ -1,21 +1,80 @@
 import socket
 
 HOST = '127.0.0.1'    # The remote host
-PORT = 57000              # The same port as used by the server
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    #Connect to Host
-    sock.connect((HOST, PORT))
+PORT = 1234              # The same port as used by the server
 
-    #Event Loop
-    while True:
-        userInput = input("Send Something to the Host: ")
-        sock.sendall(userInput.encode("utf-8"))
-        data = sock.recv(1024)
+#TESTING - Print entered host and port
+print("Host address:", HOST)
+print("Port number:", PORT)
+print()
 
-        if data.decode("utf-8").lower() == "goodbye":
-            print(f"Connection ended with {HOST}. Goodbye")
-            break
+#Establish client Socket
+cliSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        if data:
-            dataD = data.decode("utf-8")
-            print('Received:', dataD)
+#Connect to host server
+cliSock.connect((HOST, PORT))
+
+################################################
+# recvMsg - The function to handle the message #
+#             of the specified format          #
+# @param sock - socket to receive message      #
+# @returns the message without the header      #
+################################################
+def recvMsg(sock):
+    
+    # The size
+    size = sock.recv(3)
+    
+    # Convert the size to the integer
+    intSize = int(size)
+
+    # Receive the data
+    data = sock.recv(intSize)
+
+    return data
+
+#################################################
+# sendMSG - Format given message and send it to #
+#             the host server.                  #
+# @param sock - The socket to the message.      #
+# @param msg - The message to be sent.          #
+#################################################
+def sendMsg(sock, msg):
+
+    #Get length of the message
+    msgLen = str(len(msg))
+
+    #Prepend header "000" to message
+    while len(msgLen) < 3:
+        msgLen = "0" + msgLen
+
+    #Encode the message header into a byte array
+    msgLen = msgLen.encode()
+
+    #Combine message with header
+    msgSend = msgLen + msg.encode()
+
+    #Send message to host
+    sock.sendall(msgSend)
+
+#Send Username to Host
+username = input("Username: ")
+sendMsg(cliSock, username)
+
+
+#Event Loop
+while True:
+    #Send message to the host
+    userInput = input("Send Something to the Host: ")
+    sendMsg(cliSock, userInput)
+
+    #Wait for responce from host
+    data = recvMsg(cliSock)
+
+    #End connection with host
+    if data.decode() == "goodbye":
+        print(f"Connection ended with {HOST}. Goodbye.")
+        break
+
+    dataD = data.decode()
+    print('Received:', dataD)
--- Server.py
+++ Server.py
@@ -1,77 +1,116 @@
-import selectors
 import socket
-import sys
-import types
+import threading
 
-sel = selectors.DefaultSelector()
+#Host IP and Port
+listenHost = ""
+listenPort = 1234
 
-#Creates new socket and adds it to selector
-def accept_wrapper(sock):
-    conn, addr = sock.accept()
-    print(f"Accepted connection from {addr}")
-    conn.setblocking(False)
-    data = types.SimpleNamespace(addr = addr, inb = b"", outb = b"")
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    sel.register(conn, events, data = data)
+#The socket the server uses for listening
+listenSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#Handles client connections
-def service_connection(key, mask):
-    sock = key.fileobj
-    data = key.data
-    #Socket is ready to read Client's data
-    if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(1024)
-        #Data received from Client
-        if recv_data:
-            data.outb += recv_data
-        #No data received from Client
-        else:
-            print(f"Closing connection to {data.addr}")
-            sel.unregister(sock)
-            sock.close()
+#Associate the listening socket with port 1234
+listenSock.bind((listenHost, listenPort))
 
-    #Echo received data back to Client
-    if mask & selectors.EVENT_WRITE:
-        if data.outb:
-            print(f"Echoing {data.outb!r} to {data.addr}")
-            sent = sock.send(data.outb)
-            data.outb = data.outb[sent:]
+print(f"Listening on {(listenHost, listenPort)}.\n")
 
-#Check if user entered correct amount of commands
-if len(sys.argv) != 3:
-    print("ERROR: User must specify <HOST ADDRESS> and <PORT NUMBER>.")
-    print()
-    exit(-1)
+#Start listening with a connection backlog queue of 100
+listenSock.listen(100)
 
-#Get host address, and port number
-host, port = sys.argv[1], int(sys.argv[2])
+#The user name to socket dictionary
+userNameToSockDic = {}
 
-#TESTING - Print entered host and port
-print("Host address:", host)
-print("Port number:", port)
-print()
+######################################################
+# sendMsg - Puts the message into the formatted form #
+#           and sends it over the socket             #
+# @param sock - the socket to send the message       #
+# @param msg - the message                           #
+######################################################
+def sendMsg(sock, msg):
 
-#Create listening socket
-hostSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-hostSock.bind((host, port))
-hostSock.listen()
-print(f"Listening on {(host, port)}")
-hostSock.setblocking(False)
-sel.register(hostSock, selectors.EVENT_READ, data=None)
+    #Get the message length
+    msgLen = str(len(msg))
+    
+    #Keep prepending 0's until we get a header of 3 
+    while len(msgLen) < 3:
+        msgLen = "0" + msgLen
+    
+    #Encode the message into bytes
+    msgLen = msgLen.encode()
+    
+    #Put together a message
+    sMsg = msgLen + msg.encode()
+    
+    #Send the message
+    sock.sendall(sMsg)
 
-#Host Server Event Loop
-try:
-    while True:
-        events = sel.select(timeout = None)
-        for key, mask in events:
-            if key.data is None:
-                accept_wrapper(key.fileobj)
-            else:
-                service_connection(key, mask)
 
-                if key.data.outb:
-                    print(f"Testing: {key.data.outb}")
-except KeyboardInterrupt:
-    print("Caught keyboard interrupt, exiting")
-finally:
-    sel.close()
+
+##############################################################
+# The function to handle the message of the specified format #
+# @param sock - the socket to receive the message from       #
+# @returns the message without the header                    #
+##############################################################
+def recvMsg(sock):
+    
+    #The size
+    size = sock.recv(3)
+    
+    #Convert the size to the integer
+    intSize = int(size)
+
+    #Receive the data
+    data = sock.recv(intSize)
+
+    return data
+
+
+#############################################################################
+# serviceClient - Will be called by the thread that handles a single client #
+# @param clisock - the client socket                                        #
+# @param userName - the user name to serve                                  #
+#############################################################################
+def serviceClient(cliSock, userName):
+
+    # Keep servicing the client until it disconnects
+    while cliSock:
+    
+        # Receive the data from the client
+        cliData = recvMsg(cliSock)
+
+        #Check if client ended conneciton
+        if cliData.decode() == "goodbye":
+            sendMsg(cliSock, cliData.decode()) 
+            break
+    
+        print(f"Got data {cliData} from client socket {cliSock.getsockname()}")
+        
+        # Send the capitalized string to the client
+        sendMsg(cliSock, "Hello, " + userName.decode())
+    
+        # Hang up the client
+        #cliSock.close()
+    print(f"Ending connection with {cliSock.getsockname()}")
+    
+
+
+# Server loop
+while True:
+    
+    # Accept the connection
+    clienComSock, cliInfo = listenSock.accept()
+
+    print("New client connected: ", cliInfo)
+    
+    # Get the user name
+    userName = recvMsg(clienComSock)    
+    
+    print("Got user name", userName)
+    
+    # The user name to socket   
+    userNameToSockDic[userName] = clienComSock
+    
+    # Create a new thread
+    cliThread = threading.Thread(target=serviceClient, args=(clienComSock,userName,))
+    
+    # Start the thread
+    cliThread.start()
