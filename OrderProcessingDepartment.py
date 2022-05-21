47770f2839bf1048ce0f41bbd6c62285533342e2
Joshua Villasis <JJVillasis@csu.fullerton.edu>
2022-05-20 23:40:37 -0700

    Testing Order Processing

---
+++ OrderProcessingDepartment.py
@@ -0,0 +1,17 @@
+class OrderProcessingDepartment():
+
+	############################################
+	# __init__ - Class constructor for the OPD #
+	############################################
+	def __init__(self):
+		self.inventory = []
+		pass
+
+	###########################################################
+	# printInventory - Convert the inventory file to a list   #
+	###########################################################
+	def processInventory(self):
+		pass
+
+	def processOrder(self, item, amount):
+		pass
